1c4178e5d639b5f58dd2b0820474debfaa8db0f9
SyaifulArief <syaiful_arief@teknokrat.ac.id>
2026-01-13 12:13:02 +0700

    Menambahkan EmptyState

---
+++ web/src/komponen/EmptyState.tsx
@@ -0,0 +1,10 @@
+export default function EmptyState() {
+  return (
+    <div className="text-center mt-20 text-gray-500">
+      <p className="text-lg">📦 Belum ada barang</p>
+      <p className="text-sm">
+        Tambahkan barang untuk mulai barter
+      </p>
+    </div>
+  );
+}
